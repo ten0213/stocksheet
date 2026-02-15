a0a914246fce45f06fb480f12094c4c083b73fcc
황재현 <ten02130735@gmail.com>
2026-02-15 21:36:17 +0900

    백엔드 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://13.125.139.31:8088',
         changeOrigin: true,
       },
     },
